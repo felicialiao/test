3350db7d5ed4fc6dd223d65cb820535bb54f8d6f
felicialiao <79684059+felicialiao@users.noreply.github.com>
2021-03-09 17:02:33 +0800

    Add files via upload

--- ScriptIndex.js
+++ ScriptIndex.js
@@ -111,8 +111,6 @@ var xmlhttp;
 /* --- 抓會員密碼 --- (e) */
 
 /* 測試 FB 登入 API */
-
-   //初始化
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '242755040858106',
@@ -120,11 +118,11 @@ var xmlhttp;
       xfbml      : true,
       version    : 'v10.0'
     });
-    //記錄用戶行為資料 可在後台查看用戶資訊
+      
     FB.AppEvents.logPageView();   
+      
   };
-  
-//嵌入臉書sdk
+
   (function(d, s, id){
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
@@ -132,33 +130,45 @@ var xmlhttp;
      js.src = "https://connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
- 
-  $(function() {
-    //點擊登入按鈕
-    $("#login").click(function() {
-      //檢查臉書登入狀態
-      FB.getLoginStatus(function(response) {
-        //如果已經有授權過應用程式
-        if (response.authResponse) {
-          //呼叫FB.api()取得使用者資料
-          FB.api('/me',{fields: 'id,name,email'}, function (response) {
-              //這邊就可以判斷取得資料跟網站使用者資料是否一致
-          });
-        //沒授權過應用程式
-        } else {
-          //呼叫FB.login()請求使用者授權
-          FB.login(function (response) {
-            if (response.authResponse) {
-              FB.api('/me',{fields: 'id,name,email'}, function (response) {
-                //這邊就可以判斷取得資料跟網站使用者資料是否一致
-              });
-            }
-          //FB.login()預設只會回傳基本的授權資料
-          //如果想取得額外的授權資料需要另外設定在scope參數裡面
-          //可以設定的授權資料可以參考官方文件          
-          }, { scope: 'email,user_likes' });
-        }
-      });
-    });
-  });
+   
+   
+   
+async defer crossorigin="anonymous" src="https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v10.0&appId=242755040858106&autoLogAppEvents=1" nonce="F6y8hfyA">
+
+
+<fb:login-button 
+  scope="public_profile,email"
+  onlogin="checkLoginState();">
+</fb:login-button>
+
+function FBLogin() {	
+ FB.getLoginStatus(function (res) {
+                console.log(`status:${res.status}`);//Debug
+
+                if (res.status === "connected") { 
+                    let userID = res["authResponse"]["userID"];
+                    console.log("用戶已授權您的App，用戶須先revoke撤除App後才能再重新授權你的App");
+                    console.log(`已授權App登入FB 的 userID:${userID}`);
+                    GetProfile();
+                } else if (res.status === 'not_authorized' || res.status === "unknown") {
+                    //App未授權或用戶登出FB網站才讓用戶執行登入動作
+                    FB.login(function (response) {
+
+                        //console.log(response); //debug用
+                        if (response.status === 'connected') {
+                            //user已登入FB
+                            //抓userID
+                            let userID = response["authResponse"]["userID"];
+                            console.log(`已授權App登入FB 的 userID:${userID}`);
+                            GetProfile();
+
+                        } else {
+                            // user FB取消授權
+                            alert("Facebook帳號無法登入");
+                        }
+                        //"public_profile"可省略，仍然可以取得name、userID
+                    }, { scope: 'email' }); 
+                }
+            });
+}
 /* 測試 FB 登入 API */
